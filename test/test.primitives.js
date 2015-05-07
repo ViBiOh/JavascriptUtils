a17c699546f7c581646038ba133ed22fd3b1aae3
Vincent Boutour <v.boutour@omnikles.com>
2015-05-07 11:47:19 +0200

    Adding test for stringify

--- test/test.primitives.js
+++ test/test.primitives.js
@@ -481,5 +481,35 @@
         });
       });
     });
+
+    describe('stringify', function() {
+      it('should stringify a basic object', function() {
+        var result = Primitives.stringify({
+          id: 8000,
+          name: 'Bob'
+        });
+
+        expect(result).to.be.eql('{"id":8000,"name":"Bob"}');
+      });
+
+      it('should stringify with given space', function() {
+        var result = Primitives.stringify({
+          id: 8000,
+          name: 'Bob'
+        }, '  ');
+
+        expect(result).to.be.eql('{\n  "id": 8000,\n  "name": "Bob"\n}');
+      });
+
+      it('should stringify with circular reference', function() {
+        var circularObject = {
+          id: 8000
+        };
+        circularObject.child = circularObject;
+        var result = Primitives.stringify(circularObject);
+
+        expect(result).to.be.eql('{"id":8000,"child":"[Circular]"}');
+      });
+    });
   });
 })();
